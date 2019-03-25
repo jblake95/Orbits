bda3f0875d4d1ccf299ffd1c78a8d5ebc1efd289
jblake95 <J.Blake@warwick.ac.uk>
2019-03-25 22:28:57 +0000

    Slant ranges computed

--- gauss.py
+++ gauss.py
@@ -381,9 +381,18 @@ def performAlgorithm(args, obs_idx=[None, None, None]):
 	r_2 = zero # geocentric radius r_2
 	
 	# step 9 - obtain slant ranges
-	rho_1 = (1 / d_0)*(() / () - d_11)
+	num_1 = (6*(d_31*(tau_1 / tau_3) + d_21*(tau / tau_3))*r_2**3 +
+	                       MU*d_31*(tau**2 - tau_1**2)*(tau_1 / tau_3))
+	den_1 = 6*r_2**3 + MU*(tau**2 - tau_3**2)
+	num_3 = (6*(d_13*(tau_3 / tau_1) - d_23*(tau / tau_1))*r_2**3 +
+	                       MU*d_13*(tau**2 - tau_3**2)*(tau_3 / tau_1))
+	den_3 = 6*r_2**3 + MU*(tau**2 - tau_1**2)
+	
+	rho_1 = (1 / d_0)*(num_1 / den_1 - d_11)
 	rho_2 = A + MU*B / r_2**3
-	rho_3 = (1 / d_0)*(() / () - d_33)
+	rho_3 = (1 / d_0)*(num_3 / den_3 - d_33)
+	
+	print(rho_1, rho_2, rho_3)
 	
 	return np.array([])
 
