708e23071339ece325eb0fc286ea1cc42905ac40
jblake95 <J.Blake@warwick.ac.uk>
2019-07-20 17:12:25 +0100

    Much more concise config procedure

--- generateConfig.py
+++ generateConfig.py
@@ -8,6 +8,8 @@ import pprint
 
 ## TODO: Generalise for more orbital types
 TYPES = ['geo']
+POS_CHECK = ['y', 'yes']
+NEG_CHECK = ['n', 'no']
 
 def argParse():
     """
@@ -37,12 +39,16 @@ def argParse():
                              'Circular geosyncronous - "geo"',
                         type=str)
     
-    parser.add_argument('n_steps',
-                        help='Number of steps',
+    parser.add_argument('--n_steps',
+                        help='number of steps, default=1000',
                         type=int)
     
-    parser.add_argument('n_walkers',
-                        help='Number of walkers',
+    parser.add_argument('--walker_scaling',
+                        help='scaling factor for n_walkers, default=1',
+                        type=int)
+    
+    parser.add_argument('--thinning_factor',
+                        help='thinning factor for MCMC, default=1',
                         type=int)
     
     parser.add_argument('--verbose',
@@ -59,69 +65,51 @@ def generateConfig(args):
 	
 	config.update({'ephem_path':args.ephem_path})
 	config.update({'out_dir':args.out_dir})
-	config.update({'n_steps':args.n_steps})
-	config.update({'n_walkers':args.n_walkers})
+	
+	defaults = 0
+	if args.n_steps:
+		config.update({'n_steps':args.n_steps})
+	else:
+		# resort to default=1000
+		config.update({'n_steps':1000})
+		print('No n_steps provided. Using default=1000')
+		defaults += 1
+	if args.walker_scaling:
+		config.update({'walker_scaling':args.walker_scaling})
+	else:
+		# resort to default=1
+		config.update({'walker_scaling':1})
+		print('No walker_scaling provided. Using default=1')
+		defaults += 1
+	if args.thinning_factor:
+		config.update({'thinning_factor':args.thinning_factor})
+	else:
+		# resort to default=1
+		config.update({'thinning_factor':1})
+		print('No thinning_factor provided. Using default=1')
+		defaults += 1
+	if defaults > 0:
+		# check if defaults are desired
+		while True:
+			check = input('Accept the above defaults? (y|n) ')
+			if check in POS_CHECK:
+				print('Keeping defaults...')
+				break
+			elif check in NEG_CHECK:
+				print('Quitting...')
+				quit()
+			else:
+				print('Please specify "y" to keep or "n" to quit...')
 	
 	if args.orb_type in TYPES:
-		fixed = {}
-		uniform = {}
 		if args.orb_type.lower() == 'geo':
 			from params import Geo
 			g = Geo()
-			# fixed params
-			ecc = {}
-			ecc.update({'value':g.ecc_val})
-			fixed.update({'ecc':ecc})
-			
-			# uniform prior params
-			argp = {}
-			argp.update({'init':g.argp_init})
-			argp.update({'llim':g.argp_llim})
-			argp.update({'ulim':g.argp_ulim})
-			argp.update({'wght':g.argp_wght})
-			uniform.update({'argp':argp})
-			
-			raan = {}
-			raan.update({'init':g.raan_init})
-			raan.update({'llim':g.raan_llim})
-			raan.update({'ulim':g.raan_ulim})
-			raan.update({'wght':g.raan_wght})
-			uniform.update({'raan':raan})
-			
-			mmdot = {}
-			mmdot.update({'init':g.mmdot_init})
-			mmdot.update({'llim':g.mmdot_llim})
-			mmdot.update({'ulim':g.mmdot_ulim})
-			mmdot.update({'wght':g.mmdot_wght})
-			uniform.update({'mmdot':mmdot})
-			
-			mm = {}
-			mm.update({'init':g.mm_init})
-			mm.update({'llim':g.mm_llim})
-			mm.update({'ulim':g.mm_ulim})
-			mm.update({'wght':g.mm_wght})
-			uniform.update({'mm':mm})
-			
-			incl = {}
-			incl.update({'init':g.incl_init})
-			incl.update({'llim':g.incl_llim})
-			incl.update({'ulim':g.incl_ulim})
-			incl.update({'wght':g.incl_wght})
-			uniform.update({'incl':incl})
-			
-			anom = {}
-			anom.update({'init':g.anom_init})
-			anom.update({'llim':g.anom_llim})
-			anom.update({'ulim':g.anom_ulim})
-			anom.update({'wght':g.anom_wght})
-			uniform.update({'anom':anom})	
+			config.update(g.__dict__)	
 	else:
 		print('Please provide a valid orbital type:\n'
 		      'Circular geosynchronous - "geo"')
 		quit()
-	
-	config.update({'fixed':fixed})
-	config.update({'uniform':uniform})
 	
 	if args.verbose:
 		print('Config:\n'
