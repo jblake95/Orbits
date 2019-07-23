c5350029f73d56d55827f5925cafcd05c1d58c58
jblake95 <J.Blake@warwick.ac.uk>
2019-07-23 15:40:51 +0100

    Config generation working, setting up sampler

--- tlemcee.py
+++ tlemcee.py
@@ -2,8 +2,15 @@
 Applying D. Foreman-Mackey's emcee to orbit determination
 """
 
+from tle import DummyTLE
+import json
+import numpy as np
 import argparse as ap
+from collections import OrderedDict
 from astropy.table import Table
+import emcee
+import corner
+import matplotlib.pyplot as plt
 
 """
 NOTES
@@ -28,7 +35,7 @@ Fix e=0000000 to enforce circular orbit
 
 # TLE templates - non-essential elements set to zero
 TLE1 = '1 00000U 000000   {:2}{:12.8f} {:10}  00000-0  00000+0 0 00000'
-TLE2 = '2 00000 {:8.4f} {:8.4f} 0000000 {:8.4f} {:8.4f} {:11.8f}000000'
+TLE2 = '2 00000 {:8.4f} {:8.4f} {:07d} {:8.4f} {:8.4f} {:11.8f}000000'
 
 try:
     FileNotFoundError
@@ -50,43 +57,57 @@ def argParse():
     """
     parser = ap.ArgumentParser()
     
-    parser.add_argument('ephem_path',
-                        help='path to file containing ephemeris info',
+    parser.add_argument('directory',
+                        help='directory containing config file',
                         type=str)
     
-    parser.add_argument('config_path',
-                        help='path to file containing config info',
-                        type=str)
+    parser.add_argument('--threads',
+                        help='number of threads to run',
+                        type=int,
+                        default=1)
     
     return parser.parse_args()
 
-def readConfig(config_path):
+def readConfig(directory):
 	"""
 	Extract info from config file
 	
 	Parameters
 	----------
-	config_path : str
-	    Path to file containing config info
+	directory : str
+	    Directory containing config file
 	
 	Returns
 	-------
 	config : astropy Table object
 	    Table containing config info
-	"""
+	"""	
+	# load config file
 	try:
-		config = Table.read(config_path, format='csv')
+		with open('{}/config.json'.format(directory), 'r') as cfg:
+			config = json.load(cfg)
 	except FileNotFoundError:
 		print('Config file not found...')
 		quit()
 	
-	return config
+	# load ephemeris info
+	if 'ephem_path' in config.keys():
+		try:
+			ephem = Table.read(config['ephem_path'], format='csv')
+		except FileNotFoundError:
+			print('Ephemeris file not found...')
+			quit()
+	else:
+		print('Invalid config: please supply ephem_path...')
+		quit()
+	
+	return OrderedDict(config), ephem
 
-def formTLE(year, yday, mmdot, mmdot2, incl, raan, ):
+def formatTLE(year, yday, mmdot, mmdot2, incl, raan, ):
 	"""
 	Form a TLE from the given parameters
 	"""
-	
+	tle1 = TLE1.format()
 	
 	return None
 
@@ -116,7 +137,7 @@ def lnprior(theta):
 	lnprior : float
         log prior of current sample (0.0 | -np.inf)
 	"""
-	
+	mmdot, incl, raan, argp, anom, mm = theta
 	
 	return lnprior
 
@@ -134,4 +155,36 @@ if __name__ == "__main__":
 	
 	args = argParse()
 	
+	# read config info
+	config, ephem = readConfig(args.directory)
+	
+	# set up initial seed list and weights
+	initial = [config['uniform'][key]['init'] for key in config['uniform'].keys()]
+	weights = [config['uniform'][key]['wght'] for key in config['uniform'].keys()]
+	n_priors = len(initial)
+	
+	# obtain data arrays
+	x_t = np.array(ephem['utc'])
+	y_ra = np.array(ephem['ra'])
+	yerr_ra = np.array(ephem['raerr'])
+	y_dec = np.array(ephem['dec'])
+	yerr_dec = np.array(ephem['decerr'])
+	
+	####################################################################
+	###################### set up the sampler ##########################
+	####################################################################
+	n_dim = len(initial)
+	# recommended nwalkers is 4*n_parameters
+    # more walkers can help find the global minima, so optional scaling
+    n_walkers = 4*len(initial) * config['walker_scaling']
+	
+    # set up the starting positions
+    pos = [initial 
+           + weights*np.random.randn(n_dim) for i in range(n_walkers)]
+    
+    sampler = emcee.EnsembleSampler(n_walkers, n_dim, lnprob,
+                                    args=(config, n_priors,
+                                          x_lc, y_lc, yerr_lc,
+                                          x_rv, y_rv, yerr_rv),
+                                    threads=args.threads)
 	
